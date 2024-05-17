206d06fc24cb26a531363a0d1ef91a044c732607
peter <peter@paperdog.com>
2024-05-17 18:11:05 +0300

    Add HttpClientModule to app module
    The HttpClientModule is added to the import section of the app module. This is a necessary update to allow the application to make HTTP requests. The import spacing has also been normalized for better readability.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {MatCard} from "@angular/material/card";
 import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
 import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
@@ -12,29 +12,32 @@ import {MatSelect} from "@angular/material/select";
 import {MatButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        MatCard,
-        MatLabel,
-        MatFormField,
-        MatRadioButton,
-        MatRadioGroup,
-        MatOption,
-        MatSelect,
-        BrowserModule,
-        AppRoutingModule,
-        MatButton,
-        MatInput,
-        MatHint,
-        ReactiveFormsModule
-    ],
+  imports: [
+    HttpClientModule,
+    MatCard,
+    MatLabel,
+    MatFormField,
+    MatRadioButton,
+    MatRadioGroup,
+    MatOption,
+    MatSelect,
+    BrowserModule,
+    AppRoutingModule,
+    MatButton,
+    MatInput,
+    MatHint,
+    ReactiveFormsModule
+  ],
   providers: [
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
