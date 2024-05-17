e0ee35408cbccf667a57d853adb2574ed1ee3837
peter <peter@paperdog.com>
2024-05-17 18:40:49 +0300

    Add PrecalculationFormData interface
    This commit adds a new interface, PrecalculationFormData, to define the data schema for pre-calculation form inputs in the models. The interface includes parameters for membership selection, number of adults and children, status of past membership, postcode, nanny requirement, and distance.

---
+++ src/app/models/precalculation-form-data.ts
@@ -0,0 +1,9 @@
+export interface PrecalculationFormData {
+  selectedMembership: string;
+  adults: number;
+  children: number;
+  nanny: boolean;
+  pastMember: boolean;
+  postcode: string;
+  distance: number;
+}
