03e0a5c923205ddb25a54ff9441d1d0a17246eb5
peter <peter@paperdog.com>
2024-05-17 19:14:56 +0300

    Rename cost variables in costs model
    The cost variables 'monthly_cost_discounted' and 'annual_cost_discounted' have been renamed to 'monthly_cost_original' and 'annual_cost_original' respectively. This change brings more clarity to the code by accurately describing the values these variables represent.

--- src/app/models/costs.ts
+++ src/app/models/costs.ts
@@ -2,6 +2,6 @@ export interface Costs {
   entrance_fee: number;
   monthly_cost: number;
   annual_cost: number;
-  monthly_cost_discounted: number | undefined;
-  annual_cost_discounted: number | undefined;
+  monthly_cost_original?: number;
+  annual_cost_original?: number;
 }
