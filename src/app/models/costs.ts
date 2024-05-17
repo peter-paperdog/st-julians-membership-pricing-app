ea35124d3c076baa2dc8de68cf796d66748c7720
peter <peter@paperdog.com>
2024-05-17 19:02:14 +0300

    Remove entrance fee discounted field
    This commit removes the 'entrance_fee_discounted' field from the 'costs' model in src/app/models. The field was not being used and its removal does not affect the functioning of the application.

--- src/app/models/costs.ts
+++ src/app/models/costs.ts
@@ -2,7 +2,6 @@ export interface Costs {
   entrance_fee: number;
   monthly_cost: number;
   annual_cost: number;
-  entrance_fee_discounted: number | undefined;
   monthly_cost_discounted: number | undefined;
   annual_cost_discounted: number | undefined;
 }
